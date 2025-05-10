e7cc472984feca61278fed7baf79095cf083548a
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-05-10 21:18:11 +0300

    Implemented convertation the prefix expression into a Lisp-like form

--- implementation.go
+++ implementation.go
@@ -1,9 +1,62 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // TODO: document this function.
 // PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	if len(input) == 0 {
+		return "", fmt.Errorf("input is empty")
+	}
+
+	tokens := strings.Fields(input)
+	stack := []string{}
+
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
+
+		_, err := strconv.ParseFloat(token, 64)
+
+		if err == nil {
+			stack = append(stack, token)
+		} else {
+			if token == "+" || token == "-" || token == "*" || token == "/" || token == "^" {
+				if len(stack) < 2 {
+					return "", fmt.Errorf("invalid prefix expression")
+				}
+
+				operand_1 := stack[len(stack)-1]
+				operand_2 := stack[len(stack)-2]
+
+				var result string
+
+				switch token {
+				case "+":
+					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+				case "-":
+					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+				case "*":
+					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+				case "/":
+					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+				case "^":
+					result = fmt.Sprintf("(pow %s %s)", operand_1, operand_2)
+				}
+
+				stack = append(stack[:len(stack)-2], result)
+			} else {
+				return "", fmt.Errorf("invalid token: %s", token)
+			}
+		}
+	}
+
+	if len(stack) != 1 {
+		return "", fmt.Errorf("invalid prefix expression")
+	}
+
+	return stack[0], nil
 }
