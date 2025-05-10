c97aed92c0d35f2b0efd536bfcfe31e7d8da9e76
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-05-10 22:19:22 +0300

    Renamed implementation function, added tests for expressions

--- implementation.go
+++ implementation.go
@@ -6,9 +6,8 @@ import (
 	"strings"
 )
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
+// PrefixToLisp converts a prefix expression into a Lisp-style expression.
+func PrefixToLisp(input string) (string, error) {
 	if len(input) == 0 {
 		return "", fmt.Errorf("input is empty")
 	}
@@ -19,41 +18,44 @@ func PrefixToPostfix(input string) (string, error) {
 	for i := len(tokens) - 1; i >= 0; i-- {
 		token := tokens[i]
 
+		// If token is a valid number, push it to the stack
 		if isValidNumber(token) {
 			stack = append(stack, token)
-		} else {
-			if IsValidToken(token) {
-				if len(stack) < 2 {
-					return "", fmt.Errorf("invalid prefix expression")
-				}
-
-				operand_1 := stack[len(stack)-1]
-				operand_2 := stack[len(stack)-2]
-
-				var result string
-
-				switch token {
-				case "+":
-					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
-				case "-":
-					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
-				case "*":
-					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
-				case "/":
-					result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
-				case "^":
-					result = fmt.Sprintf("(pow %s %s)", operand_1, operand_2)
-				}
-
-				stack = append(stack[:len(stack)-2], result)
-			} else {
-				return "", fmt.Errorf("invalid token: %s", token)
+		} else if IsValidToken(token) {
+			// If token is a valid operator, pop operands from the stack
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid expression: not enough operands for operator %s", token)
 			}
+
+			operand_1 := stack[len(stack)-1]
+			operand_2 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2] // Remove the two operands from the stack
+
+			// Format the result based on the operator
+			var result string
+			switch token {
+			case "+":
+				result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+			case "-":
+				result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+			case "*":
+				result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+			case "/":
+				result = fmt.Sprintf("(%s %s %s)", token, operand_1, operand_2)
+			case "^":
+				result = fmt.Sprintf("(pow %s %s)", operand_1, operand_2)
+			}
+
+			// Push the resulting expression back onto the stack
+			stack = append(stack, result)
+		} else {
+			return "", fmt.Errorf("invalid token: %s", token)
 		}
 	}
 
+	// At the end, there should only be one element in the stack: the final Lisp expression
 	if len(stack) != 1 {
-		return "", fmt.Errorf("invalid prefix expression")
+		return "", fmt.Errorf("invalid expression: many operands in final stack: %v", stack)
 	}
 
 	return stack[0], nil
--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestPrefixToLisp(t *testing.T) {
+	// Simple expressions with 2-3 operands
+	test1, err := PrefixToLisp("+ 5 * - 4 2 3")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, "(+ 5 (* (- 4 2) 3))", test1)
 	}
+
+	test2, err := PrefixToLisp("+ 2 3")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(+ 2 3)", test2)
+	}
+
+	// Hard expressions with more operands
+	test3, err := PrefixToLisp("+ 5 * - 4 2 ^ 3 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(+ 5 (* (- 4 2) (pow 3 2)))", test3)
+	}
+
+	test4, err := PrefixToLisp("* + 2 2 3")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(* (+ 2 2) 3)", test4)
+	}
+
+	test5, err := PrefixToLisp("+ * 2 + 3 4 5")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(+ (* 2 (+ 3 4)) 5)", test5)
+	}
+
+	// Hard expressions with 7-10 operands
+	test6, err := PrefixToLisp("+ 1 + 2 + 3 + 4 + 5 + 6 + 7 8")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(+ 1 (+ 2 (+ 3 (+ 4 (+ 5 (+ 6 (+ 7 8)))))))", test6)
+	}
+
+	test7, err := PrefixToLisp("+ * + 2 2 3 + + 4 5 + 6 7")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(+ (* (+ 2 2) 3) (+ (+ 4 5) (+ 6 7)))", test7)
+	}
+
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+func ExamplePrefixToLisp() {
+	res, _ := PrefixToLisp("+ 5 * - 4 2 ^ 3 2")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// (+ 5 (* (- 4 2) (pow 3 2)))
 }
